6475f51064ae6f6e270c1100887a8980a54ac702
FiveNizza <tema.prohoroff@gmail.com>
2016-06-01 17:09:39 +0300

    Example for employment monitor

---
+++ src/MyExample/PaymentRunner.java
@@ -0,0 +1,22 @@
+package MyExample;
+
+public class PaymentRunner {
+    public static void main(String[] args) throws InterruptedException {
+        final Payment payment = new Payment();
+        new Thread() {
+            public void run() {
+                payment.doPayment();
+            }
+        }.start();
+        Thread.sleep(200);
+        synchronized (payment) {
+            System.out.println("Init amount");
+            payment.initAmount();
+            payment.notify();
+        }
+        synchronized (payment) {
+            payment.wait(1000);
+            System.out.println("ok");
+        }
+    }
+}
