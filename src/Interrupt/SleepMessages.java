e17039a2983ca579f4179bfd4ec83b23beadb3dd
FiveNizza <tema.prohoroff@gmail.com>
2016-06-01 11:39:27 +0300

    Thread sleep with try/catch

---
+++ src/Interrupt/SleepMessages.java
@@ -0,0 +1,20 @@
+package Interrupt;
+
+public class SleepMessages {
+    public static void main(String[] args) {
+        String importantInfo[] = {
+                "Mares eat oats",
+                "Does eat oats",
+                "Little lambs eat ivy",
+                "A kid will eat ivy too"
+        };
+        for (int i = 0; i < importantInfo.length; i++) {
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                return;
+            }
+            System.out.println(importantInfo[i]);
+        }
+    }
+}
